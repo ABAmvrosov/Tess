8abb6de4c84a07220cf8327fa179c6dfa84c5ed5
ABAmvrosov <abamvrosov@gmail.com>
2016-05-19 10:20:54 +0300

    MainMenu added

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,56 @@
+﻿/*Base script taken from Game Development for Modern Platforms MSU course*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenuManager : MonoBehaviour {
+	
+	// Submenus
+	public GameObject mainMenu;
+	//public GameObject optionsMenu;
+	//public GameObject aboutMenu;
+	public Text titleText;
+
+	private string _mainTitle;
+
+	void Awake()
+	{
+		_mainTitle = titleText.text;
+		// determine if Quit button should be shown
+		ShowMenu("mainMenu");
+	}
+
+	public void ShowMenu(string name)
+	{
+		mainMenu.SetActive (false);
+		//aboutMenu.SetActive(false);
+		//optionsMenu.SetActive(false);
+
+		switch(name) {
+		case "mainMenu":
+			mainMenu.SetActive (true);
+			titleText.text = _mainTitle;
+			break;
+		case "optionsMenu":
+			//optionsMenu.SetActive(true);
+			titleText.text = "Options";
+			break;
+		case "About":
+			//aboutMenu.SetActive(true);
+			titleText.text = "About";
+			break;
+		}
+	}
+
+	public void loadLevel(string leveltoLoad)
+	{
+		Application.LoadLevel (leveltoLoad);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+}
